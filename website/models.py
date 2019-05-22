1500ffb72e2266cc29d75523619b7dd1e57b9a03
Julia Schulz <s8juschu@stud.uni-saarland.de>
2019-05-22 21:40:50 +0200

    new plan started

--- website/models.py
+++ website/models.py
@@ -7,3 +7,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE, default=None)
     username = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.username
